73687b5565a9845169f1d928dedfd27b574a1477
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-19 21:56:37 -0500

    Some changes to the player physics, making them use a rigidbody. Still broken, and causes the player to fly frequently

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class Player : MonoBehaviour
 {
-    private CharacterController characterController;
+    new private Rigidbody rigidbody;
+    private Collision LastCollision;
     public GameObject PlayerCamera;
     public float MovementSpeed;
     public float MouseSensitivity;
@@ -15,13 +16,12 @@ public class Player : MonoBehaviour
 
     private float CameraYaw = 0.0f;
     private float CameraPitch = 0.0f;
-    private float VerticalVelocity = 0.0f;
-    private bool JumpedThisTick = false;
     
     // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        rigidbody = GetComponent<Rigidbody>();
+        LastCollision = null;
         // keep cursor in center of frame
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -35,7 +35,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    
     void FixedUpdate()
     {        
         // adjust the angle of the camera
@@ -45,27 +44,27 @@ public class Player : MonoBehaviour
         // feed the new angle to the main camera
         PlayerCamera.transform.rotation = Quaternion.Euler(-CameraPitch, CameraYaw, 0);
         // rotate the player around the vertical axis only
-        transform.rotation = Quaternion.Euler(0.0f, CameraYaw, 0.0f);
-        // apply gravity and jumping force
-        if (!JumpedThisTick && characterController.isGrounded) {
-            VerticalVelocity = -0.1f;
-        } else {
-            VerticalVelocity -= Gravity * Time.deltaTime;
-        }
+        rigidbody.rotation = Quaternion.Euler(0.0f, CameraYaw, 0.0f);
         // use WASD to determine horizontal displacement od character
         Vector3 horizontalMovement = transform.right * Input.GetAxisRaw("Horizontal") + transform.forward * Input.GetAxisRaw("Vertical");
-        // move the character and add in gravity
-        characterController.Move((horizontalMovement * MovementSpeed + Vector3.up * VerticalVelocity) * Time.deltaTime);
+        rigidbody.AddForce(horizontalMovement * MovementSpeed, ForceMode.VelocityChange);
         if (IsOutOfBounds()) {
             transform.position = new Vector3(0.0f, 10.0f, 0.0f);
+            rigidbody.AddForce(-rigidbody.velocity, ForceMode.VelocityChange);
         }
-        JumpedThisTick = false;
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        LastCollision = collision;
+    }
+
+    void OnCollisionExit() {
+        LastCollision = null;
     }
 
     public void Jump(float force) {
-        if (characterController.isGrounded) {
-            VerticalVelocity = force;
-            JumpedThisTick = true;
+        if (LastCollision != null) {
+            rigidbody.AddForce(Vector3.up * force, ForceMode.VelocityChange);
             Debug.Log("Player jumped with force " + force);
         } else {
             Debug.Log("Player tried to jump but wasn't grounded");
